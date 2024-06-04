183606536648da8c477cc7f76a9fe3c5ed758155
siddiquechengapra <siddiquechengapra@gmail.com>
2024-06-05 00:22:22 +0530

    registration complete

--- alerty/api/index.js
+++ alerty/api/index.js
@@ -6,16 +6,24 @@ const crypto = require("crypto");
 const app = express();
 const port = 3000;
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const jwt = require("jsonwebtoken");
+const generateSecretKey = () => {
+    const secretKey = crypto.randomBytes(32).toString("hex");
+  
+    return secretKey;
+  };
+  
+  const secretKey = generateSecretKey();
+
 // const moment = require("moment");
 
 mongoose
-    .connect("mongodb+srv://siddiquechengapra:siddiquechengapra@cluster0.qcivovq.mongodb.net/")
+    .connect("mongodb+srv://siddiquechengapra:siddiquechengapra@cluster0.qcivovq.mongodb.net/alertyDB")
     .then(() => {
         console.log("Connected to MongoDB");
     })
@@ -26,3 +34,54 @@ mongoose
 app.listen(port, () => {
     console.log("Server is running on port 3000");
 });
+
+const User = require("./models/user");
+const Todo = require("./models/todo");
+
+app.post("/register", async (req, res) => {
+    console.log("coming here ")
+    try {
+      const { name, email, password } = req.body;
+  
+      ///check if email is already registered
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        console.log("Email already registered");
+      }
+  
+      const newUser = new User({
+        name,
+        email,
+        password,
+      });
+  
+      await newUser.save();
+  
+      res.status(202).json({ message: "User registered successfully" });
+    } catch (error) {
+      console.log("Error registering the user", error);
+      res.status(500).json({ message: "Registration failed" });
+    }
+  });
+
+  app.post("/login", async (req, res) => {
+    try {
+      const { email, password } = req.body;
+  
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: "Invalid Email" });
+      }
+  
+      if (user.password !== password) {
+        return res.status(401).json({ message: "Invalide password" });
+      }
+  
+      const token = jwt.sign({ userId: user._id }, secretKey);
+  
+      res.status(200).json({ token });
+    } catch (error) {
+      console.log("Login failed", error);
+      res.status(500).json({ message: "Login failed" });
+    }
+  });
--- alerty/app/(authenticate)/register.js
+++ alerty/app/(authenticate)/register.js
@@ -1,26 +1,80 @@
 import React, { useState } from 'react';
-import { View, Text, SafeAreaView, KeyboardAvoidingView, TextInput, Pressable } from 'react-native';
+import { View, Text, SafeAreaView, KeyboardAvoidingView, TextInput, Pressable, Alert } from 'react-native';
 import { AntDesign, MaterialIcons } from '@expo/vector-icons'
 import { useRouter } from "expo-router";
+import axios from "axios";
 export default function register() {
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [name, setName] = useState("")
+
     const router = useRouter()
+
+    const handleRegister = () => {
+        const user = { 
+            name,
+            email,
+            password
+        }
+        console.log('User object before sending:', user);
+
+        axios.post("http://192.168.1.3:3000/register",user).then((response) => {
+            console.log(response);
+            Alert.alert("Registration successfull","You have been registered succesfully");
+            setEmail("");
+            setPassword("");
+            setName("");
+        }).catch((error) => {
+            Alert.alert("Registration failed","an error ocurred during registration");
+            console.log("error",error)
+        })
+    }
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: "white", alignItems: "center" }}>
 
             <View style={{ marginTop: 80 }}>
-                <Text style={{ fontSize: 18, fontWeight: "500", color: "green" }}>Todo List Tracker</Text>
+                <Text style={{ fontSize: 18, fontWeight: "500", color: "green" }}>Maalu's Tracker</Text>
             </View>
             <KeyboardAvoidingView>
                 <View style={{ alignItems: "center" }}>
                     <Text style={{ fontSize: 16, fontWeight: "600", marginTop: 20 }}>Register your account</Text>
                 </View>
-                <View style={{ marginTop: 70 }}>
+                <View
+                    style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                        gap: 5,
+                        backgroundColor: "#E0E0E0",
+                        paddingVertical: 5,
+                        borderRadius: 5,
+                        marginTop: 30,
+                    }}
+                >
+                    <AntDesign
+                        style={{ marginLeft: 8 }}
+                        name="user"
+                        size={24}
+                        color="gray"
+                    />
+                    <TextInput
+                        value={name}
+                        onChangeText={(text) => setName(text)}
+                        style={{
+                            color: "gray",
+                            marginVertical: 10,
+                            width: 300,
+                            fontSize: email ? 17 : 17,
+                        }}
+                        placeholder="enter your name"
+                    />
+                </View>
+                
+                <View style={{ marginTop: 30 }}>
+                    
                     <View style={{ flexDirection: "row", alignItems: "center", gap: 5, backgroundColor: "#E0E0E0", paddingVertical: 5, borderRadius: 5, marginTop: "30" }}>
                         <MaterialIcons style={{ marginLeft: 8, color: "gray" }} name="email" size={24} color="black" />
-                        <TextInput value={email} onChange={(text) => setEmail(text)} style={{ color: "gray", marginVertical: 10, width: 300, fontSize: email ? 17 : 18 }} placeholder='Enter your email' />
+                        <TextInput value={email} onChangeText={(text) => setEmail(text)} style={{ color: "gray", marginVertical: 10, width: 300, fontSize: email ? 17 : 18 }} placeholder='Enter your email' />
                     </View>
                 </View>
                 <View
@@ -57,7 +111,7 @@ export default function register() {
                 <View style={{ marginTop: 60 }} />
 
                 <Pressable
-                    // onPress={handleLogin}
+                    onPress={handleRegister}
                     style={{
                         width: 200,
                         backgroundColor: "#6699CC",
